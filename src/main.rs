8971ab1aa5c9c02e8b47590a57013fa33d094dec
carlca <carl.caulkett@gmail.com>
2023-12-20 23:01:10 +0000

    Trying to get read_single_key to work.

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,90 @@
+extern crate termion;
+
 use std::io;
-use std::io::Read;
 use std::io::Write;
+use termion::input::TermRead;
+use termion::event::Key;
 
 enum Number {
-    Int(i32),
-    Float(f64),
+  Int(i32),
+  Float(f64),
 }
 
 fn main() {
-    let operand1 = get_operand("Operand 1: ");
-    let operand2 = get_operand("Operand 2: ");
-    let mut operator = enter_operator("Operator (+,-,*,/): ");
-    match operator {
-        '+' => println!("Result is: {}", operand1 + operand2),
-        '-' => println!("Result is: {}", operand1 - operand2),
-        '*' => println!("Result is: {}", operand1 * operand2),
-        '/' => {
-            let result = if operand2 == 0.0 {
-                "NAN".to_string()
-            } else {
-                (operand1 / operand2).to_string()
-            };
-            println!("Result is: {}", result)},
-        _ => println!("Incorrect operator selected!")
-    };
+  loop {
+    calculate();
+    let msg = "press Q or q to quit or any other key to continue...";
+    println!("{}", msg);
+    let key = read_single_key();
+    match key {
+      Some('q') | Some('Q') => break,
+      _ => (),
+    }
+  }
+}
+
+fn calculate() {
+  let operand1 = get_operand("Operand 1: ");
+  let operand2 = get_operand("Operand 2: ");
+  let operator = enter_operator("Operator (+,-,*,/): ");
+  match operator {
+    Some('+') => println!("Result is: {}", operand1 + operand2),
+    Some('-') => println!("Result is: {}", operand1 - operand2),
+    Some('*') => println!("Result is: {}", operand1 * operand2),
+    Some('/') => {
+      let result = if operand2 == 0.0 {
+        "NAN".to_string()
+      } else {
+        (operand1 / operand2).to_string()
+      };
+      println!("Result is: {}", result)
+    }
+    _ => println!("Incorrect operator selected!"),
+  };
 }
 
 fn get_operand(prompt: &str) -> f64 {
-    match enter_operand(prompt) {
-        Number::Int(i) => i as f64,
-        Number::Float(f) => f,
-    }
+  match enter_operand(prompt) {
+    Number::Int(i) => i as f64,
+    Number::Float(f) => f,
+  }
 }
 
 fn enter_operand(prompt: &str) -> Number {
-    print!("{}", prompt);
-    io::stdout().flush().unwrap();use std::io::Write;
-    let mut operand = String::new();
-    io::stdin().read_line(&mut operand).expect("Failed to read_line!");
-    let trimmed = operand.trim();
-    match trimmed.parse::<i32>() {
-        Ok(i) => Number::Int(i),
-        Err(_) => match trimmed.parse::<f64>() {
-            Ok(f) => Number::Float(f),
-            Err(_) => panic!("Please enter a number!"),
-        },
-    }
+  print!("{}", prompt);
+  io::stdout().flush().unwrap();
+  let mut operand = String::new();
+  io::stdin()
+    .read_line(&mut operand)
+    .expect("Failed to read_line!");
+  let trimmed = operand.trim();
+  match trimmed.parse::<i32>() {
+    Ok(i) => Number::Int(i),
+    Err(_) => match trimmed.parse::<f64>() {
+      Ok(f) => Number::Float(f),
+      Err(_) => panic!("Please enter a number!"),
+    },
+  }
 }
 
-fn enter_operator(prompt: &str) -> char {
-    let mut stdin = io::stdin();
-    let mut buffer = [0; 1];
-    print!("{}", prompt);
-    io::stdout().flush().unwrap();
-    stdin.read_exact(&mut buffer).unwrap();
-    let operator = buffer[0] as u8 as char;
-    match operator {
-        '+' => println!("Add selected."),
-        '-' => println!("Subtract selected."),
-        '*' => println!("Multiply selected."),
-        '/' => println!("Divide selected."),
-        _ => println!("Incorrect operator selected!")
-    }
-    io::stdout().flush().unwrap();use std::io::Write;
-    operator
+fn enter_operator(prompt: &str) -> Option<char> {
+  print!("{}", prompt);
+  io::stdout().flush().unwrap();
+  match read_single_key() {
+    Some('+') => { println!("Add selected."); Some('+')},
+    Some('-') => { println!("Subtract selected."); Some('-')},
+    Some('*') => { println!("Multiply selected."); Some('*')},
+    Some('/') => { println!("Divide selected."); Some('/')},
+    Some(_) => { println!("Incorrect operator selected!"); None},
+    None => None,
+  }
+}
+
+fn read_single_key() -> Option<char> {
+  let stdin = io::stdin();
+  let mut stdin_keys = stdin.lock().keys();
+  match stdin_keys.next().and_then(|result| result.ok()) {
+    Some(Key::Char(c)) => Some(c),
+    _ => None,
+  }
 }
